2ece69efe99f94f6c7fd9c5c3ac30e2c5eb71d26
ank0chiya <freedom.love.is.crazy@gmail.com>
2024-04-14 02:54:38 +0000

    first commit

---
+++ src/routes/Material.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import Button from "@smui/button";
+    import Card from "@smui/card";
+</script>
+
+<main>
+    <h1>test</h1>
+    <Button variant="raised">hoge</Button>
+    <Card padded>hoge</Card>
+</main>
