99736035a922421c2f7d683169928c632a7b8874
ank0chiya <freedom.love.is.crazy@gmail.com>
2024-06-08 14:05:10 +0000

    update

--- src/components/Archivement.svelte
+++ src/components/Archivement.svelte
@@ -1,8 +1,12 @@
-<script>
+<script lang="ts">
     import Card, { Content } from "@smui/card";
     import List, { Item, Text } from "@smui/list";
+    import Checkbox from "@smui/checkbox";
+    import FormField from "@smui/form-field";
 
     import { data } from "../lib/archivements";
+    import { writable, get } from "svelte/store";
+    import { onDestroy } from 'svelte';
 
     import arch from "../data/archivement.json";
 
@@ -10,13 +14,41 @@
         (a, b) => a[1].order - b[1].order,
     );
 
-    function clickHandler(d) {
-        console.log(`in click handler: ${JSON.stringify(d)}`)
-        $data = d
-    } 
-    $: console.log(`in Archivemente svelte: ${JSON.stringify($data)}`);
-</script>
+    function load() {
+        console.log(`data: ${data}`)
+        sortedAchievements.map((d) => {
 
+            let archivements = d[1].achievements.map((a) => {
+                if (Array.isArray(a)) {
+                    a.forEach((ad) => {
+                        ad.checked = writable(false);
+                    });
+                }   
+                a.checked = writable(false);
+                return a
+            })
+            return d
+        })
+
+        $data = sortedAchievements
+    }
+
+    load()
+
+    const list = writable([])
+
+    function clickHandler(d) {
+        $list = d[1].achievements
+    }
+    function handleCheck(d) {
+        let checked = d.checked;
+        if (get(checked) == true) {
+            checked.set(false);
+        } else {
+            checked.set(true);
+        }
+    }
+</script>
 
 <div
     class="scrollbar scrollbar-corner-rounded-full scrollbar-track-rounded-full scrollbar-thumb-slate-300 scrollbar-track-slate-300 card-display overflow-y-auto h-screen"
@@ -24,8 +56,11 @@
     <div class="card-container">
         <Card>
             <Content component={List}>
-                {#each sortedAchievements as archivements}
-                    <Item on:click={() => clickHandler(archivements[1].achievements)}>
+                {#each $data as archivements}
+                    <Item
+                        on:click={() =>
+                            clickHandler(archivements)}
+                    >
                         {archivements[1].name}
                     </Item>
                 {/each}
@@ -33,22 +68,65 @@
         </Card>
     </div>
 </div>
-
-
 <div class="flex gap-4">
-    <div class="flex-col">
-        {#each $data as d}
+    <div class="flex-col scrollbar scrollbar-corner-rounded-full scrollbar-track-rounded-full scrollbar-thumb-slate-300 scrollbar-track-slate-300 card-display overflow-y-auto h-screen">
+        {#each $list as d}
             {#if d instanceof Array}
-                {#each d as _d}
-                    <div>
-                        {JSON.stringify(_d)}
-                    </div>
-                {/each}
+                <Card>
+                    {#each d as _d}
+                        <Content class="divide-y">
+                            <div class="flex">
+                                <div class="flex-initial w-64 text-left">
+                                    <div>
+                                        {_d.name}
+                                        <code class="bg-slate-200 rounded"
+                                            >{_d.ver}</code
+                                        >
+                                    </div>
+                                    <div>
+                                        {_d.desc}
+                                    </div>
+                                </div>
+                                <div class="">
+                                    <FormField>
+                                        <Checkbox
+                                            checked={get(_d.checked)}
+                                            on:change={() => handleCheck(_d)}
+                                        />
+                                    </FormField>
+                                </div>
+                            </div>
+                        </Content>
+                    {/each}
+                </Card>
             {:else}
-                <div>
-                    {JSON.stringify(d)}
-                </div>
+                <Card>
+                    <Content class="divide-y">
+                        <div class="flex">
+                            <div class="text-left">
+                                <div>
+                                    {d.name}
+                                    <code class="bg-slate-200 rounded"
+                                        >{d.ver}</code
+                                    >
+                                </div>
+                                <div>
+                                    {d.desc}
+                                </div>
+                            </div>
+                            <div>
+                                <FormField>
+                                    <Checkbox
+                                        checked={get(d.checked)}
+                                        on:change={() => handleCheck(d)}
+                                    />
+                                </FormField>
+                            </div>
+                        </div>
+                    </Content>
+                </Card>
             {/if}
         {/each}
     </div>
 </div>
+
--- src/lib/archivements.js
+++ src/lib/archivements.js
@@ -1,3 +1,11 @@
 import { writable } from 'svelte/store';
 
+import arch from "../data/archivement.json";
+
 export const data = writable([]);
+
+
+export const archData = writable({});
+
+
+console.log(`archdata in arch.js ${archData}`)
